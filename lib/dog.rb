2981d3775c722af13fcd72ca7059886782cf9f05
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-07 14:59:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,12 @@ class Dog
 
   def initialize(name, breed)
     @name = name
+    @breed = breed 
   end
 
   def breed=(breed=("Mutt"))
     @breed = breed
+  end
 
   def name=(name)
     @name = name
