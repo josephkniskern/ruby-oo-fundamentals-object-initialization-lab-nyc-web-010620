37587b332a970b27fed51162b3f233756e3e5be5
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-07 14:54:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,13 @@
 class Person
   def initialize(name)
-    @name = name 
+    @name = name
   end
 
   def name=(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
-  def name 
-    @name 
-  end 
+  def name
+    @name
+  end
 end
-    
